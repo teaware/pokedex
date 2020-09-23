e9a2cf8679944fba2be4eb7d909578f54e7c1e4b
George Brown <teawares@gmail.com>
2020-09-23 20:56:01 +0800

    margin

--- components/color.js
+++ components/color.js
@@ -5,7 +5,7 @@ function Color({ name }) {
   const { data } = useSWR(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
 
   return (
-    <div className="my-2 p-2 w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4">
+    <div className="p-2 w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4">
       <article
         className={`rounded-md shadow-md w-full px-2 bg-${
           data ? data.color.name : "gray"
