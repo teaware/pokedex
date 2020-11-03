a33bb2d47fbe117176afa8210b09f2dcd9d5d145
George Brown <teawares@gmail.com>
2020-11-03 17:01:15 +0800

    getStaticProps()

--- pages/[name].js
+++ pages/[name].js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const easing = [0.175, 0.85, 0.42, 0.96];
 const backVariants = {
@@ -8,8 +8,8 @@ const backVariants = {
     opacity: 0,
     transition: {
       duration: 0.5,
-      ease: easing
-    }
+      ease: easing,
+    },
   },
   enter: {
     y: 0,
@@ -17,9 +17,9 @@ const backVariants = {
     transition: {
       delay: 0.5,
       duration: 0.5,
-      ease: easing
-    }
-  }
+      ease: easing,
+    },
+  },
 };
 
 function Post({ pokemon, pokemonSpecies }) {
@@ -31,72 +31,88 @@ function Post({ pokemon, pokemonSpecies }) {
   const theName = pokemonSpecies ? lan[0].name : name;
 
   return (
-    <motion.div initial="exit" animate="enter" exit="exit" variants={backVariants}>
-    <div
-      className={`poke-detail p-4 h-screen w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto bg-${bgc}-500`}
-      style={{ maxHeight: 812 }}
+    <motion.div
+      initial="exit"
+      animate="enter"
+      exit="exit"
+      variants={backVariants}
     >
-      
-      <div className="flex justify-between h-12 items-center px-1">
-        <Link href="/">
-          <a className="cursor-pointer rounded-full h-10 w-10 flex items-center justify-center text-white text-2xl bg-gray-700">
-            ←
-          </a>
-        </Link>
-        <h2 className="font-mono text-2xl capitalize">{pokemon.name}</h2>
-        <div className="font-mono text-lg">#{pokemon.id}</div>
-      </div>
-      <div className="text-center my-4">
-        <div className="w-40 h-40 m-auto">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-          alt={pokemon.name}
-        />
+      <div
+        className={`poke-detail p-4 h-screen w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto bg-${bgc}-500`}
+        style={{ maxHeight: 812 }}
+      >
+        <div className="flex justify-between h-12 items-center px-1">
+          <Link href="/" scroll={false}>
+            <a className="cursor-pointer rounded-full h-10 w-10 flex items-center justify-center text-white text-2xl bg-gray-700">
+              ←
+            </a>
+          </Link>
+          <h2 className="font-mono text-2xl capitalize">{pokemon.name}</h2>
+          <div className="font-mono text-lg">#{pokemon.paddedId}</div>
         </div>
-        <div className="text-5xl opacity-50">{theName}</div>
-        <div className="my-2">
-          {pokemon.types.map((type) => (
-            <span
-              key={type.type.name}
-              className="inline-block bg-gray-400 bg-opacity-25 rounded-lg px-2 text-sm text-gray-700 mr-2 mb-2"
-            >
-              {type.type.name}
-            </span>
-          ))}
+        <div className="text-center my-4">
+          <div className="w-40 h-40 m-auto">
+            <img src={pokemon.officialArtwork} alt={pokemon.name} />
+          </div>
+          <div className="text-5xl opacity-50">{theName}</div>
+          <div className="my-2">
+            {pokemon.types.map((type) => (
+              <span
+                key={type.type.name}
+                className="inline-block bg-gray-400 bg-opacity-25 rounded-lg px-2 text-sm text-gray-700 mr-2 mb-2"
+              >
+                {type.type.name}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="rounded-md bg-white px-4 py-2 divide-y-2 divide-gray-500 divide-dotted">
-        <div className="grid grid-cols-2 py-2">
-          <div className="text-center">
-            <span>身高</span>: {pokemon.height / 10} m
-          </div>
-          <div className="text-center">
-            <span>體重</span>: {pokemon.weight / 10} kg
-          </div>
-        </div>
-        <div className="py-2">
-          {pokemon.stats.map((stat) => (
-            <div key={stat.stat.name} className="py-1">
-              <span className="uppercase">{stat.stat.name}</span>:{" "}
-              {stat.base_stat}
+        <div className="rounded-md bg-white px-4 py-2 divide-y-2 divide-gray-500 divide-dotted">
+          <div className="grid grid-cols-2 py-2">
+            <div className="text-center">
+              <span>身高</span>: {pokemon.height / 10} m
             </div>
-          ))}
+            <div className="text-center">
+              <span>體重</span>: {pokemon.weight / 10} kg
+            </div>
+          </div>
+          <div className="py-2">
+            {pokemon.stats.map((stat) => (
+              <div key={stat.stat.name} className="py-1">
+                <span className="uppercase">{stat.stat.name}</span>:{" "}
+                {stat.base_stat}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      
-    </div>
     </motion.div>
   );
 }
 
-// This gets called on every request
-export async function getServerSideProps({ query }) {
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=893");
+  const posts = await res.json();
+
+  const paths = posts.results.map((post) => ({
+    params: { name: post.name },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+// This function gets called at build time
+export async function getStaticProps({ params }) {
   // params contains the post `id`.
-  const { name } = query;
   // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
   const pokemon = await res.json();
+  pokemon.paddedId = ("00" + pokemon.id).slice(-3);
+  pokemon.officialArtwork = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
 
   const res2 = await fetch(pokemon.species.url);
   const pokemonSpecies = await res2.json();
