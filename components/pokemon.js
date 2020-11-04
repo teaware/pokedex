19751816fdd215878dd52f31dc1e3bc474e28798
George Brown <teawares@gmail.com>
2020-11-05 00:06:14 +0800

    motion

--- components/pokemon.js
+++ components/pokemon.js
@@ -50,13 +50,16 @@ function Pokemon({ expanded, setExpanded, name }) {
           <AnimatePresence initial={false}>
             {isOpen && (
               <motion.div
+                className="fixed inset-0"
                 key="content"
                 initial="exit"
                 animate="enter"
                 exit="exit"
-                className="fixed inset-0"
               >
-                <div className="w-full h-screen bg-black opacity-75"></div>
+                <motion.div
+                  className="w-full h-screen bg-black"
+                  variants={mask}
+                ></motion.div>
                 <div className="fixed inset-0 w-full h-screen flex justify-center items-center">
                   <motion.div
                     className={`poke-detail p-4 h-screen w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto bg-${bgc}-500`}
@@ -136,7 +139,7 @@ function Pokemon({ expanded, setExpanded, name }) {
             )}
           </AnimatePresence>
         ) : (
-          <p>We lost connection</p>
+          ""
         )}
       </div>
     </AnimateSharedLayout>
@@ -147,8 +150,33 @@ export default Pokemon;
 
 const easing = [0.175, 0.85, 0.42, 0.96];
 
+const mask = {
+  exit: {
+    opacity: 0,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+      ease: [0.04, 0.62, 0.23, 0.98],
+    },
+  },
+  enter: {
+    opacity: 0.75,
+    transition: {
+      duration: 0.5,
+      ease: [0.04, 0.62, 0.23, 0.98],
+    },
+  },
+};
+
 const imageVariants = {
-  exit: { y: 150, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  exit: {
+    y: 150,
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: easing,
+    },
+  },
   enter: {
     y: 0,
     delay: 0.5,
@@ -161,20 +189,32 @@ const imageVariants = {
 };
 
 const textVariants = {
-  exit: { y: 100, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  exit: {
+    y: 100,
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: easing,
+    },
+  },
   enter: {
     delay: 0.5,
     y: 0,
     opacity: 1,
-    transition: { delay: 0.1, duration: 0.5, ease: easing },
+    transition: {
+      delay: 0.1,
+      duration: 0.5,
+      ease: easing,
+    },
   },
 };
 
 const backVariants = {
   exit: {
-    x: 100,
+    x: "100%",
     opacity: 0,
     transition: {
+      delay: 0.5,
       duration: 0.5,
       ease: easing,
     },
