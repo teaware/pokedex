56f17eb29cfc5d5cfe68ac800029a3e2704231fd
George Brown <teawares@gmail.com>
2021-03-04 01:19:31 +0800

    🤏🏻 mind the gap

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ function CatchEmAll() {
             </div>
           </div>
         ) : (
-          <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {pokemonList.map((pokemon) => {
               return pokemon.results.map((result) => (
                 <Pokemon
