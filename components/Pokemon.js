587c5be1210144dca08f1aca53d2a59aa7b54a5e
George Brown <teawares@gmail.com>
2021-03-04 13:15:27 +0800

    👨🏻‍🔧 pokemon cards

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -13,38 +13,40 @@ export default function Pokemon({ name, id }) {
   const theName = pokemonSpecies ? lan[0].name : name;
 
   return (
-    <div className="rounded-md shadow-md w-full p-2 bg-white">
+    <>
       {pokemon ? (
-        <Link href={`/${id}`} scroll={false}>
-          <a>
-            <div className="poke-name flex justify-between items-center px-1">
-              <div>
-                <h2 className="text-lg capitalize mb-2">{theName}</h2>
+        <div className="rounded-md shadow-md w-full p-2 bg-white">
+          <Link href={`/${id}`} scroll={false}>
+            <a>
+              <div className="poke-name flex justify-between items-center px-1">
                 <div>
-                  {pokemon.types.map((type) => (
-                    <span
-                      key={type.type.name}
-                      className="inline-block bg-gray-400 bg-opacity-25 rounded-lg px-2 text-sm text-gray-700 mr-2 mb-2"
-                    >
-                      {type.type.name}
-                    </span>
-                  ))}
+                  <h2 className="text-lg capitalize mb-1">{theName}</h2>
+                  <div>
+                    {pokemon.types.map((type) => (
+                      <span
+                        key={type.type.name}
+                        className="inline-block bg-gray-400 bg-opacity-25 rounded-lg px-2 text-sm text-gray-700 mr-2 mb-2"
+                      >
+                        {type.type.name}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <div className="w-16 h-16 ml-1">
+                  <Image
+                    src={pokemon.sprites.front_default}
+                    alt={name}
+                    width="96"
+                    height="96"
+                  />
                 </div>
               </div>
-              <div className="w-16 h-16 ml-1">
-                <Image
-                  src={pokemon.sprites.front_default}
-                  alt={name}
-                  width="64"
-                  height="64"
-                />
-              </div>
-            </div>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </div>
       ) : (
-        <p className="font-bold text-l capitalize">Loading {name}...</p>
+        <div className="h-20" />
       )}
-    </div>
+    </>
   );
 }
